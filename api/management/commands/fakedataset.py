3c66ca4948a443050d2f8b818f7474a94adb91ed
shekhar-singh <sschoudhary60@gmail.com>
2020-08-16 18:16:19 +0530

    added exception handler for negative args

--- api/management/commands/fakedataset.py
+++ api/management/commands/fakedataset.py
@@ -2,33 +2,35 @@ from django.core.management.base import BaseCommand
 from api.models import User , ActivityPeriod
 from datetime import datetime, timedelta
 from django.utils import timezone
+import argparse
 from faker import Faker
 
-faker = Faker()	
+faker = Faker() 
 
 class Command(BaseCommand):
     help = 'Create random users and ActivityPeriod'
 
     def add_arguments(self, parser):
-        parser.add_argument('total', type=int, help='Indicates the number of users and activity to be created')
+        parser.add_argument('total', type=check_positive, help='Indicates the number of users to be created')
 
     def handle(self, *args, **kwargs):
         total = kwargs['total']
+
         for i in range(total):
-        	try:
-        		user_id=faker.md5()[::4].upper()
-        		User.objects.create(user_id=user_id,real_name=faker.name(),tz=faker.timezone())
+            user_id=faker.md5()[::4].upper()
+            User.objects.create(user_id=user_id,real_name=faker.name(),tz=faker.timezone())
 
-        		ac=faker.random_int(1,5)
-        		
-        		for j in range(ac):
-        			start_time=faker.date_time()
-        			ActivityPeriod.objects.create(User_id=user_id,start_time=start_time, end_time=start_time+timedelta(hours=1))
+            ac=faker.random_int(1,5)
+            
+            for j in range(ac):
+                start_time=faker.date_time()
+                ActivityPeriod.objects.create(User_id=user_id,start_time=start_time, end_time=start_time+timedelta(hours=1))
 
-        		#self.stdout.write("%s Activity created succesfully" % ac)
-
-
-        	except:
-        		pass
-
+       
         self.stdout.write("%s fakedataset created succesfully" % total)
+
+def check_positive(value):
+    ivalue = int(value)
+    if ivalue <= 0:
+        raise argparse.ArgumentTypeError("%s is an invalid positive int value" % value)
+    return ivalue
